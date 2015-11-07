3bfbd9b90ae7f81cddf036be383f6532b0ac2d2b
Gabriel Simmer <bladesimmer@gmail.com>
2015-11-07 11:59:38 -0800

    Featching info from Voat done, working on styling.

--- js/GetHomePage.js
+++ js/GetHomePage.js
@@ -3,9 +3,16 @@ $(document).ready(function(){
 });
 
 function Get(){
-  var result;
-  $.get("http://gabrielsimmer.com/moat-mobile/api/GET.php",{sv:"all"}).done(function(data){
+  $.get("http://gabrielsimmer.com/moat-mobile/api/GET.php").done(function(data){
     result = JSON.parse(data);
-    $(".posts").html(data);
+    for (var i = 0; i < result.length; i++){
+      $(".posts").append('<div class="row"> <div class="col-md-8">'+
+      '<h3><a href="'+result[i]['MessageContent']+'">' + result[i]['Linkdescription'] + '</a></h3>'+
+      '<h5 class="moreinfo">Comments: '+result[i]['CommentCount']+
+      ' Dislikes: '+result[i]['Dislikes']+
+      ' Likes: ' + result[i]['Likes'] +
+      '</h5>'+
+      '</div></div>')
+    }
   });
 }
